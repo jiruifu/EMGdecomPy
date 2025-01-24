919e2054ff23a9c1c6d2554d4713c53849cdf435
Jirui Fu <jiruif@hotmail.com>
2025-01-23 23:23:37 -0500

    update

--- emgdecompy_wenlab/__init__.py
+++ emgdecompy_wenlab/__init__.py
@@ -2,6 +2,6 @@ from .preprocessing import *
 from .contrast import *
 from .decomposition import *
 from .viz import *
+from .file_utils import *
 
-
-__all__ = ['preprocessing', 'contrast', 'decomposition', 'viz']
+__all__ = ['preprocessing', 'contrast', 'decomposition', 'viz', 'file_process']
